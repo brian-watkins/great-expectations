40b4ce86af64a73479432939033124539fbcfde6
Brian Watkins <brian.watkins@gmail.com>
2022-11-27 14:52:49 -0500

    Match a string containing a value some number of times

--- behaviors/isStringContaining.behavior.ts
+++ behaviors/isStringContaining.behavior.ts
@@ -1,4 +1,4 @@
-import { behavior } from "esbehavior";
+import { behavior, outcome } from "esbehavior";
 import { isStringContaining } from "../src";
 import { exhibit, hasExpectedMessage, hasInvalidActual, hasMessage, isInvalidMatchResult, isValidMatchResult } from "./helpers";
 
@@ -37,5 +37,39 @@ export default behavior("isStringContaining", [
   }).check([
     isInvalidMatchResult(),
   ]),
+
+  exhibit("when the value contains the string the expected number of times", () => {
+    return isStringContaining("is", { times: 3 })("This is a fish!")
+  }).check([
+    isValidMatchResult()
+  ]),
+
+  exhibit("when the value does not contain the string the expected number of times", () => {
+    return isStringContaining("is", { times: 3 })("This is a bat!")
+  }).check([
+    isInvalidMatchResult(),
+    hasExpectedMessage("A string containing 'is' exactly 3 times")
+  ]),
+
+  exhibit("when the value does not contain the string only once", () => {
+    return isStringContaining("is", { times: 1 })("This is a bat!")
+  }).check([
+    isInvalidMatchResult(),
+    hasExpectedMessage("A string containing 'is' exactly 1 time")
+  ]),
+
+  exhibit("when the expected has regexp special characters in it", () => {
+    return isStringContaining("f.sh", { times: 1 })("fish, f.sh, fosh")
+  }).check([
+    isValidMatchResult()
+  ]),
+
+  exhibit("when the expectation tries to match a negative number of times", () => {
+    return isStringContaining("f.sh", { times: -11 })("fish, f.sh, f.sh")
+  }).check([
+    outcome("it works just like if the times value were not set", [
+      isValidMatchResult()
+    ])
+  ])
 
 ])
--- src/index.ts
+++ src/index.ts
@@ -110,28 +110,65 @@ interface ArrayMatchMessage {
   message: string
 }
 
-export function isStringContaining(val: string, options: StringContainingOptions = {}): Matcher<string> {
+export function isStringContaining(expected: string, options: StringContainingOptions = {}): Matcher<string> {
   const isCaseSensitive = options.caseSensitive ?? true
+  const matchCount = options.times ?? -1
 
   return (actual) => {
     let actualValue = actual
-    let expectedValue = val
+    let expectedValue = expected
     if (!isCaseSensitive) {
       actualValue = actual.toLowerCase()
-      expectedValue = val.toLowerCase()
+      expectedValue = expected.toLowerCase()
     }
 
-    if (actualValue.includes(expectedValue)) {
+    const count = getStringMatchCount(actualValue, expectedValue)
+
+    if ((matchCount < 0 && count > 0) || (matchCount >= 0 && count == matchCount)) {
       return new Valid()
     } else {
       return new Invalid("The actual value does not contain the expected string.", {
         actual: invalidActualValue(actual),
-        expected: expectedMessage(`A string containing '${val}'${isCaseSensitive ? '' : ' (case-insensitive)' }`)
+        expected: expectedMessage(stringInvalidMessage(expected, isCaseSensitive, matchCount))
       })
     }
   }
 }
 
+function stringInvalidMessage(expected: string, isCaseSensitive: boolean, matchCount: number): string {
+  let message = `A string containing '${expected}'`
+
+  if (!isCaseSensitive) {
+    message += " (case-insensitive)"
+  }
+
+  if (matchCount == 1) {
+    message += " exactly 1 time"
+  }
+
+  if (matchCount == 0 || matchCount > 1) {
+    message += ` exactly ${matchCount} times`
+  }
+
+  return message
+}
+
+function getStringMatchCount(message: string, term: string): number {
+  let searchIndex = 0
+  let count = 0
+  while (true) {
+    const index = message.indexOf(term, searchIndex)
+    if (index > 0) {
+      count++
+      searchIndex = index + 1
+    } else {
+      break
+    }
+  }
+  return count
+}
+
 interface StringContainingOptions {
   caseSensitive?: boolean
+  times?: number
 }
