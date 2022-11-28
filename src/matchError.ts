70d16427ccd7f4e7580a321d696532202e7111ac
Brian Watkins <brian.watkins@gmail.com>
2022-11-28 06:58:48 -0500

    Custom errors have proper name
    - and add suggested fix for working with stack
    traces in V8

--- src/matchError.ts
+++ src/matchError.ts
@@ -4,6 +4,12 @@ import { stringify } from "./stringify"
 export class MatchError extends Error {
   constructor(private invalid: Invalid, ...args: any) {
     super(invalid.description, ...args)
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, MatchError)
+    }
+
+    this.name = "MatchError"
   }
 
   get actual(): string {
