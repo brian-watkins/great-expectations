1483e0c79a3019c7f261f8600fb6b67d951185f1
Brian Watkins <brian.watkins@gmail.com>
2023-02-05 14:20:54 -0500

    Export types for creating custom matchers at top level

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ export { stringMatching, stringWithLength, stringContaining } from "./stringMatc
 export type { StringContainingOptions } from "./stringMatchers"
 export { satisfying } from "./satisfyingMatchers"
 export { objectWithProperty, objectWith } from "./objectMatchers"
-
+export { Valid, Invalid } from "./matcher"
+export type { Matcher, MatchResult, MatchValues } from "./matcher"
 
 export function expect<T, S>(value: T, evaluator: MatchEvaluator<T, S>, description?: string): S {
   return evaluator(value, description)
