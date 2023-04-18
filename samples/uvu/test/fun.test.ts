d9800dd4ac93fb987248f3061f4eadb74347f505
Brian Watkins <brian.watkins@gmail.com>
2023-04-17 20:26:10 -0400

    Sample with uvu

---
+++ samples/uvu/test/fun.test.ts
@@ -0,0 +1,27 @@
+import { test } from "uvu"
+import { arrayWith, arrayWithLength, equalTo, expect, is, mapWith } from "../../../src/index.js"
+
+interface Item {
+  id: number
+  title: string
+}
+
+test("This is a test", () => {
+  const map = new Map<string, Array<Item>>()
+  map.set("Team 1", [
+    { id: 7, title: "Item 7" },
+    { id: 4, title: "Item 4" }
+  ])
+  map.set("Team 2", [
+    { id: 9, title: "Item 9" }
+  ])
+
+  expect(map, is(mapWith([
+    {
+      key: equalTo("Team 3"),
+      // value: arrayWithLength(0)
+    }
+  ])))
+})
+
+test.run()
